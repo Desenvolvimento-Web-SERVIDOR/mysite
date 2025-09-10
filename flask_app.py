d305657d83ef1521196e401deaf32abf05bac1d8
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-09-09 21:52:34 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -2,11 +2,18 @@ from datetime import datetime
 from flask import Flask, render_template, request, make_response, redirect, abort
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired 
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators= [DataRequired()])
+    submit = SubmitField('Submit')
 
 @app.route('/')
 def index():
